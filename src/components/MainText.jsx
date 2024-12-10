031fe42fc70030c0e3c453dda97e1060079b4d5f
atharvguptaaa <workinatharv@gmail.com>
2024-12-10 16:09:54 +0530

    c

---
+++ src/components/MainText.jsx
@@ -0,0 +1,42 @@
+import { TypeAnimation } from 'react-type-animation';
+
+function MainText() {
+  return (
+    <>
+
+<div className="flex justify-center sm:justify-start">
+  <div className="justify-center sm:justify-start mt-8 p-6 ml-4 sm:ml-0 text-3xl sm:text-6xl md:text-8xl whitespace-nowrap">
+    <TypeAnimation
+      sequence={[
+        "<Atharv Gupta>",
+        1000,
+      ]}
+      wrapper="span"
+      speed={1}
+      style={{  display: "inline-block" }}
+      repeat={Infinity}
+    />
+  </div>
+</div>
+
+<div className=" mt-2 md:mt-8  flex justify-center sm:justify-start">
+  <div className=" p-6 text-2xl sm:text-4xl md:text-6xl whitespace-nowrap">
+    <TypeAnimation
+      className="delay-2000"
+      sequence={[
+        "<Full-Stack Dev>",
+        1000,
+      ]}
+      wrapper="span"
+      speed={1}
+      style={{ display: "inline-block" }}
+      repeat={Infinity}
+    />
+  </div>
+</div>    
+
+    </>
+  )
+}
+
+export default MainText
